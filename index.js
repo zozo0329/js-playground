5aa19da062522a7b8f177dd8c8389ff52660960f
zozo0329 <renzosantos0329@gmail.com>
2024-03-28 09:56:28 +0800

    fixed

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ let no3 = document.getElementById("btn3");
 let no4 = document.getElementById("btn4");
 
 // AUDIO
+
 let boom = new Audio();
 boom.src = "audio/BOOM sound effect.mp3";
 let poop = new Audio();
 poop.src = "poop.mp3";
+
 // IMAGES
+
 let img1 = document.getElementById("img1");
 let img2 = document.getElementById("img2");
 let img3 = document.getElementById("img3");
 let img4 = document.getElementById("img4");
+
 // FUNCTIONS
+
 function ohNo() {
   no.style.visibility = "hidden";
   no2.style.visibility = "visible";
